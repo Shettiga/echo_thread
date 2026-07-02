f5cedc764d3a4f73a285e071377800e794b15b84
Shettiga <shettigarhemashreesudarshan@srinivasuniversity.edu.in>
2026-07-02 21:33:13 +0530

    There was a changes in config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,5 @@
 class AppConfig {
   // Use http://192.168.1.26:5000 for physical phone debug on local Wi-Fi.
   // Use http://10.0.2.2:5000 for Android Emulator.
-  // Change to your Render URL once deployed.
-  static const String backendUrl = 'http://192.168.1.26:5000';
+  static const String backendUrl = 'https://echo-thread.onrender.com';
 }
